ab2466db59327821389874106da40e043a32c06b
tapas <tapaskumarpaul84@gmail.com>
2024-02-14 20:00:19 +0530

    New function added

---
+++ demo.py
@@ -0,0 +1,5 @@
+def sayhi():
+    a=5
+    b=7
+    print("a+b=",a+b)
+    return "Hello"
--- test.py
+++ test.py
@@ -1,2 +1,6 @@
+from demo import sayhi
 print("Hello world")
 print("This is git and jenkin tutorial")
+
+a=sayhi()
+print(a)
